8660c617300ac84adbb0a032db2fdf1b48389d1c
jsrikrishna11 <41172782+jsrikrishna11@users.noreply.github.com>
2019-08-20 19:34:04 +0530

    Update advancedDataStructures.R

--- advancedDataStructures.R
+++ advancedDataStructures.R
@@ -1,5 +1,9 @@
+#Data frames
 x <- 10:1
 y <- -4:5
 q <- c("Hockey","Football", "Baseball", "Curling", "Rugby","Lacrosse", "Basketball", "Tennis", "Cricket", "Soccer")
 theDF <- data.frame(x,y,q)
 theDF <- data.frame(First = x, Second = y, Sport = q)
+
+
+#lists
