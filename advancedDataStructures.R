bb5499ad87c186f4e97e947cb0acbd1b786a3160
jsrikrishna11 <41172782+jsrikrishna11@users.noreply.github.com>
2019-08-20 13:20:12 +0530

    Create advancedDataStructures.R

---
+++ advancedDataStructures.R
@@ -0,0 +1,5 @@
+x <- 10:1
+y <- -4:5
+q <- c("Hockey","Football", "Baseball", "Curling", "Rugby","Lacrosse", "Basketball", "Tennis", "Cricket", "Soccer")
+theDF <- data.frame(x,y,q)
+theDF <- data.frame(First = x, Second = y, Sport = q)
