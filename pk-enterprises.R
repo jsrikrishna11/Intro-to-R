2503f21eb580694e665cd51fbd828b8166a26294
jsrikrishna11 <41172782+jsrikrishna11@users.noreply.github.com>
2019-09-05 10:30:03 +0530

    End-to-end
    Still need to increase the flexibility of the code.
    I will have to make a function out of this piece of code with required inputs and also provide specs to the kind of inputs it can take.

--- pk-enterprises.R
+++ pk-enterprises.R
@@ -1,23 +1,20 @@
 setwd("C:\\Users\\purna\\Desktop\\Coding\\R\\Datasets\\prithvi-krishna-enterprises")
 wb = loadWorkbook("july-2019-purchase.xlsx")
 entry = readWorksheet(wb, sheet = "Sheet1", header = TRUE)
-suppliers = entry$Name
-
-#frequency of each distributor by code
-# library(ggplot2)
-# library(ggpubr)
-# theme_set(theme_pubr())
-# qplot(as.numeric(suppliers), geom="histogram", binwidth= 1)
-#average value of each purchase per supplier
-supplier_list = unique(suppliers)
+supplier_list = unique(entry$Name)
 #creating a matrix  with rows as suppliers and columns as purchases
-supplier_purchases = list()
+supplier_purchases = matrix(ncol = 4, byrow = TRUE)
+index = 0
 for (i in supplier_list){
   index <- which(entry$Name==i, arr.ind = TRUE)
   addition = sum(entry[index,]$NetAmt)
   purchaseCount = length(index)
   average = addition/purchaseCount
-  supplier_purchases[[i]] = c(average, addition,  purchaseCount)
+  supplier_purchases = rbind(supplier_purchases, c(as.character(i),average, addition,  purchaseCount))
 }
-#converting supplier_purchases list to dataframe
+supplier_purchases = na.exclude(supplier_purchases)
+colnames(supplier_purchases)= c("SupplierName", "AveragePurchases", "TotalAmount", "NumberofPurchases")
 
+#writing supplier_purchases matrix into a csv file
+
+write.csv(as.data.frame(supplier_purchases),file = "Extraction.csv")
